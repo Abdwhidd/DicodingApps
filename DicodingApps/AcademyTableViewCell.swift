59ee566a2545ab634097a08e6573d8c7baa421c5
Abdul Wahid Kahar <76991016+Abdwhidd@users.noreply.github.com>
2022-06-17 10:52:34 +0700

    Add files via upload

---
+++ DicodingApps/AcademyTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  AcademyTableViewCell.swift
+//  DicodingApps
+//
+//  Created by Wahid on 17/06/22.
+//
+
+import UIKit
+
+class AcademyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var academyLabel: UILabel!
+    @IBOutlet weak var academyImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ DicodingApps/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  DicodingApps
+//
+//  Created by Wahid on 17/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var academyTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        academyTableView.dataSource = self
+        academyTableView.register(
+            UINib(nibName: "AcademyTableViewCell", bundle: nil),
+            forCellReuseIdentifier: "AcademyCell"
+        )
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyAcademyData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(
+          withIdentifier: "AcademyCell",
+          for: indexPath
+        ) as? AcademyTableViewCell {
+            // Mencari UITableViewCell berdasarkan Identifier.
+            let academy = dummyAcademyData[indexPath.row]
+            cell.academyLabel.text = academy.name
+            cell.academyImageView.image = academy.image
+          return cell
+        } else {
+          return UITableViewCell() // Mengembalikan UITableViewCell jika tidak ditemukan.
+        }
+    }
+    
+    
+}
